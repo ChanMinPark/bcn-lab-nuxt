86ba26d2abc2f1c83e124f5fa1c4405d9fd3f4ef
pcm <walkinpcm@gmail.com>
2018-01-06 02:13:59 +0900

    교수님 소개 내용 채움.

--- pages/member/professor/index.vue
+++ pages/member/professor/index.vue
@@ -1,6 +1,119 @@
 <template>
-  <div id="member-professor">
-    멤버 > 교수님
+  <div id="member-professor" class="container">
+    <h1 class="page-title">Professor</h1>
+    
+    <section class="professor-info">
+      <div class="profile-image">
+        <img src="/img/professor/prof-picture.jpg" alt="Dr. Byung-Seo Kim">
+      </div>
+      <div class="info">
+        <h2 class="name">Dr. Byung-Seo Kim (김병서)</h2>
+        <div class="position">
+          Associate Professor<br>
+          Department of Computer & Information Communication Engineering<br>
+          Hongik University<br>
+          Sejong City, KOREA
+        </div>
+        <div class="contact-channel">
+          Phone: +82-41-860-2539 Fax: +82-41-865-0460<br>
+          Email: jsnbs@hongik.ac.kr<br>
+          Homepage: http://shinan.hongik.ac.kr/~jsnbs/
+        </div>
+      </div>
+    </section>
+
+    <hr>
+
+    <section class="professor-brief-bio">
+      <h3>Brief Bio.</h3>
+      <div>
+        Prof. Byung-Seo Kim received the B.S. degree in electrical engineering from In-Ha University, InCheon, Korea in 1998 and received M.S. and Ph.D. degrees in electrical and computer engineering from University of Florida in 2001 and 2004, respectively. During Ph.D degree, he has been advised by Professor Yuguang "Michael" Fang in Wireless Networks Group (WINET).<br>
+        Between 1997 and 1999, he worked for Motorola Korea Ltd., PaJu, Korea as a Computer Integrated Manufacturing (CIM) Engineer in ATR&D department.<br>
+        From January 2005 to August 2007, he has been with Motorola Inc. Schaumburg Illinois as a Sr. Software Engineer in Networks & Enterprise, Motorola Inc.<br>
+        From September 2007, he has been with Hongik University - Sejong Campus as an Assistant Professor in Department of Computer & Information Communication Engineering.<br>
+        He is an IEEE Senior Member.<br>
+        He have been investigating and researching emerging wireless technologies for public safety networks. My research interests include in designing and developing of efficient wireless MAC protocol, Wireless ICN/CCN/NDN, Mobile Edge Computing, cross layer architecture, Broadband PLC, and resource allocation algorithm for wireless networks.<br>
+      </div>
+    </section>
+    
+    <hr>
+
+    <section class="professor-professional-activities">
+      <h3>Professional Activities</h3>
+      <div>
+        <ul>
+          <li>Guest Editor for Special Issue on "Cross-layer Design Issues, Challenges and Opportunities for Future Intelligent Heterogeneous Networks,"<br>Journal of Ambient Intelligence and Humanized Computing, 2017</li>
+          <li>Guest Editor for Special Issue on Resource Management in Vehicular Ad-Hoc Networks: Energy Management, Communication Protocol and Future Applications, IEEE Access, 2016</li>
+          <li>Lead-Guest Editor for Annual Special Issue on A Breakthrough in Multi-Hop Wireless Multimedia Sensor Networking Protocols, International Journal of Distributed Sensor Networks, from 2016 ~ Present</li>
+          <li>General Chair of 3rd IWWCN 2017</li>
+          <li>TPC Member of ICGHIC 2016, 2017 and 2018</li>
+          <li>TPC Member of ICNISC 2017 and 2018</li>
+          <li>TPC Member of EAI FUTURE 2016 conferences</li>
+          <li>TPC Member of the IEEE VTC 2014-Spring</li>
+          <li>Member of Sejongsi Construction Consideration Committee(세종특별자치시 건설심의 위원회) 2013 ~ 2015</li>
+          <li>Member of Asan-si Design Consultative Committee(아산시 설계자문위원회), 2012 ~ 2014</li>
+          <li>Member of Proposal Evaluation Board in National Health Insurance Service. (국민건강 보험공단 제안서평가위원) 2012 ~ 2014</li>
+        </ul>
+      </div>
+    </section>
+    
+    <hr>
+
+    <section class="professor-invited-talk">
+      <h3>Invited Talk</h3>
+      <div>
+        <ul>
+          <li>2017.09.30 "Wireless Information-Centric Networking with Edge Computing for Vehicular Applications" APNOMS-2017</li>
+          <li>2017.08.21 "ICT in Smart City" Government Official Training Workshop, Academy of Managers for Construction and Cities in Hanoi, Vietnam.</li>
+          <li>2016.12.02 "5G & Edge Cloud Computing" KNOM Tutorial 2016</li>
+          <li>2016.11.23 "Edge/Fog Computing" KREONET Workshop 2016</li>
+          <li>2016.07.17 "Mobile/Wireless ICN for Edge Networking" Future Internet Forum Edge Computing/Networking Mini WS</li>
+          <li>2012.07.13 "Info-mechatronics Convergence Course" 5th ABEEK Capstone Design Workshop</li>
+          <li>2011.10.22 "Cross Layer Design over Ad-Hoc Networks" National University of Defense Technology in China</li>
+          <li>2011.09.28 "Point Coordination Function (PCF) & Its Enhanced Variants" Electronics and Telecommunications Research Institute</li>
+          <li>2010.12.02 "Technologies for Future Tactical Wireless Networks" R.O.K. Joint Chiefs of Staff</li>
+          <li>2010.10.08 "Wireless Ad-hoc Networks" Electronics and Telecommunications Research Institute (ETRI)</li>
+          <li>2009.06.16 "Wired/Wireless Networks for Vehicular Communications" Korea Aerospace Research Institute</li>
+          <li>2009.03.04 "Link-Adaptable Protocols for wireless Networks" Konkuk University</li>
+          <li>2008.06.24 "MAC for Tactical Communication Networks" Korea Internet Conference (KRnet) 2008</li>
+          <li>2007.11.27 "Link-Adaptable Medium Access Control Protocols for High-Speed wireless Networks" Ajou University</li>
+        </ul>
+      </div>
+    </section>
+    
+    <hr>
+
+    <section class="professor-research-interests">
+      <h3>Research Interests</h3>
+      <div>
+        <ul>
+          <li>Medium Access Control (MAC) Protocols in Wireless/Wired Networks (IEEE 802.11x, 802.15.x, 802.16.x, 802.20, LTE)</li>
+          <li>Cross Layer Design (Physical and MAC Layers) for wireless networks</li>
+          <li>5th Generation Communication/Network Technologies, LTE-Unlicensed Spectrum</li>
+          <li>Mobile/Wireless Content Centric Networks (CCN)</li>
+          <li>Future Internet and Home Automation Technologies</li>
+          <li>Power Line Communication</li>
+        </ul>
+      </div>
+    </section>
+
+    
+    <hr>
+
+    <section class="professor-student">
+      <h3>
+        Perspective Graduate and Post Doctor Student<br>
+        (Including Foreign Students)
+      </h3>
+      <div>
+        <ul>
+          <li>I am always looking for self-motivated, easy-going graduate students who are strongly committed to research. The successful candidates are expected     to conduct research in wireless/mobile networking and Security, and smart grid.</li>
+          <li>Minimum requirement(none for Post Doctor Student): TOEIC 750 or TOEFL-CBT 197 or TEPS 625 or IELTS 6.0</li>
+          <li>Support: tuition and living expense (For more information for foreign graduate students, click "<a href="http://grad.hongik.ac.kr/subpage/foreigner.php?id=2" target="_blank">Global Scholarship for Foreign Graduate Students</a>")</li>
+          <li>Please, send me a mail with your resume.</li>
+        </ul>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -9,4 +122,4 @@
   }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" src="./professor.less" scoped></style>
