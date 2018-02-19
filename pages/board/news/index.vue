f7feeaa69e1fa24bd38a090c14d716eeb999322b
ChanMinPark <walkinpcm@gmail.com>
2018-02-19 16:43:01 +0000

    news 리스트 샘플 구현

--- pages/board/news/index.vue
+++ pages/board/news/index.vue
@@ -1,6 +1,66 @@
 <template>
-  <div id="board-news">
-    게시판 > 뉴스
+  <div id="board-news" class="container">
+    <h1 class="page-title">Board - News</h1>
+    
+    <section>
+      <ul class="row list-unstyled news-list">
+        <li class="col-sm-4">
+          <div class="news-card">
+            <!-- 썸네일 이미지 -->
+            <div class="news-thumbnail" :style="{ backgroundImage: `url('/img/member/chanmin.JPG')` }"></div>
+            <!-- 뉴스 타이틀 -->
+            <div class="news-title">
+              <h3>[새소식] 홈페이지가 리뉴얼 되었습니다.</h3>
+            </div>
+            <div class="news-date">2018.03.01</div>
+          </div>
+        </li>
+        <li class="col-sm-4">
+          <div class="news-card">
+            <!-- 썸네일 이미지 -->
+            <div class="news-thumbnail" :style="{ backgroundImage: `url('/img/member/chanmin.JPG')` }"></div>
+            <!-- 뉴스 타이틀 -->
+            <div class="news-title">
+              <h3>[새소식] 홈페이지가 리뉴얼 되었습니다.</h3>
+            </div>
+            <div class="news-date">2018.03.01</div>
+          </div>
+        </li>
+        <li class="col-sm-4">
+          <div class="news-card">
+            <!-- 썸네일 이미지 -->
+            <div class="news-thumbnail" :style="{ backgroundImage: `url('/img/member/chanmin.JPG')` }"></div>
+            <!-- 뉴스 타이틀 -->
+            <div class="news-title">
+              <h3>[새소식] 홈페이지가 리뉴얼 되었습니다.</h3>
+            </div>
+            <div class="news-date">2018.03.01</div>
+          </div>
+        </li>
+        <li class="col-sm-4">
+          <div class="news-card">
+            <!-- 썸네일 이미지 -->
+            <div class="news-thumbnail" :style="{ backgroundImage: `url('/img/member/chanmin.JPG')` }"></div>
+            <!-- 뉴스 타이틀 -->
+            <div class="news-title">
+              <h3>[새소식] 홈페이지가 리뉴얼 되었습니다.</h3>
+            </div>
+            <div class="news-date">2018.03.01</div>
+          </div>
+        </li>
+        <li class="col-sm-4">
+          <div class="news-card">
+            <!-- 썸네일 이미지 -->
+            <div class="news-thumbnail" :style="{ backgroundImage: `url('/img/member/chanmin.JPG')` }"></div>
+            <!-- 뉴스 타이틀 -->
+            <div class="news-title">
+              <h3>[새소식] 홈페이지가 리뉴얼 되었습니다.</h3>
+            </div>
+            <div class="news-date">2018.03.01</div>
+          </div>
+        </li>
+      </ul>
+    </section>
   </div>
 </template>
 
@@ -9,4 +69,4 @@
   }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" src="./news.less" scoped></style>
