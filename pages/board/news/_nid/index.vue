0f2c1f0f7eaf81bc5bd176b07791d8ad1894782a
ChanMinPark <walkinpcm@gmail.com>
2018-02-24 15:01:02 +0000

    news 상세 페이지 퍼블리싱

---
+++ pages/board/news/_nid/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="news-detail" class="container">
+    <h1 class="page-title">Board - News</h1>
+    
+    <section class="news-detail-section">
+      <h2 class="news-title">[새소식] 홈페이지가 리뉴얼 되었습니다.</h2>
+      <div class="news-date">2018.03.01</div>
+      
+      <hr>
+      
+      <div v-html="'<p>안녕하세요.<br>홈페이지가 리뉴얼 되었습니다.<br>얏호~ :)<p>'"></div>
+      
+      <hr>
+      
+      <div class="text-right">
+        <nuxt-link class="btn btn-primary" :to="{ name: 'board-news' }">목록보기</nuxt-link>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+  
+</script>
+
+<style lang="less" src="./detail.less" scoped></style>
--- pages/board/news/index.vue
+++ pages/board/news/index.vue
@@ -5,59 +5,95 @@
     <section>
       <ul class="row list-unstyled news-list">
         <li class="col-sm-4">
-          <div class="news-card">
-            <!-- 썸네일 이미지 -->
-            <div class="news-thumbnail" :style="{ backgroundImage: `url('/img/member/chanmin.JPG')` }"></div>
-            <!-- 뉴스 타이틀 -->
-            <div class="news-title">
-              <h3>[새소식] 홈페이지가 리뉴얼 되었습니다.</h3>
+          <nuxt-link :to="{ name: 'board-news-nid', params: { nid: 1 }}">
+            <div class="news-card">
+              <!-- 썸네일 이미지 -->
+              <div class="news-thumbnail" :style="{ backgroundImage: `url('/img/member/chanmin.JPG')` }"></div>
+              <!-- 뉴스 타이틀 -->
+              <div class="news-title">
+                <h3>[새소식] 홈페이지가 리뉴얼 되었습니다.</h3>
+              </div>
+              <div class="news-date">2018.03.01</div>
             </div>
-            <div class="news-date">2018.03.01</div>
-          </div>
+          </nuxt-link>
         </li>
         <li class="col-sm-4">
-          <div class="news-card">
-            <!-- 썸네일 이미지 -->
-            <div class="news-thumbnail" :style="{ backgroundImage: `url('/img/member/chanmin.JPG')` }"></div>
-            <!-- 뉴스 타이틀 -->
-            <div class="news-title">
-              <h3>[새소식] 홈페이지가 리뉴얼 되었습니다.</h3>
+          <nuxt-link :to="{ name: 'board-news-nid', params: { nid: 1 }}">
+            <div class="news-card">
+              <!-- 썸네일 이미지 -->
+              <div class="news-thumbnail" :style="{ backgroundImage: `url('/img/member/chanmin.JPG')` }"></div>
+              <!-- 뉴스 타이틀 -->
+              <div class="news-title">
+                <h3>[새소식] 홈페이지가 리뉴얼 되었습니다.</h3>
+              </div>
+              <div class="news-date">2018.03.01</div>
             </div>
-            <div class="news-date">2018.03.01</div>
-          </div>
+          </nuxt-link>
         </li>
         <li class="col-sm-4">
-          <div class="news-card">
-            <!-- 썸네일 이미지 -->
-            <div class="news-thumbnail" :style="{ backgroundImage: `url('/img/member/chanmin.JPG')` }"></div>
-            <!-- 뉴스 타이틀 -->
-            <div class="news-title">
-              <h3>[새소식] 홈페이지가 리뉴얼 되었습니다.</h3>
+          <nuxt-link :to="{ name: 'board-news-nid', params: { nid: 1 }}">
+            <div class="news-card">
+              <!-- 썸네일 이미지 -->
+              <div class="news-thumbnail" :style="{ backgroundImage: `url('/img/member/chanmin.JPG')` }"></div>
+              <!-- 뉴스 타이틀 -->
+              <div class="news-title">
+                <h3>[새소식] 홈페이지가 리뉴얼 되었습니다.</h3>
+              </div>
+              <div class="news-date">2018.03.01</div>
             </div>
-            <div class="news-date">2018.03.01</div>
-          </div>
+          </nuxt-link>
         </li>
         <li class="col-sm-4">
-          <div class="news-card">
-            <!-- 썸네일 이미지 -->
-            <div class="news-thumbnail" :style="{ backgroundImage: `url('/img/member/chanmin.JPG')` }"></div>
-            <!-- 뉴스 타이틀 -->
-            <div class="news-title">
-              <h3>[새소식] 홈페이지가 리뉴얼 되었습니다.</h3>
+          <nuxt-link :to="{ name: 'board-news-nid', params: { nid: 1 }}">
+            <div class="news-card">
+              <!-- 썸네일 이미지 -->
+              <div class="news-thumbnail" :style="{ backgroundImage: `url('/img/member/chanmin.JPG')` }"></div>
+              <!-- 뉴스 타이틀 -->
+              <div class="news-title">
+                <h3>[새소식] 홈페이지가 리뉴얼 되었습니다.</h3>
+              </div>
+              <div class="news-date">2018.03.01</div>
             </div>
-            <div class="news-date">2018.03.01</div>
-          </div>
+          </nuxt-link>
         </li>
         <li class="col-sm-4">
-          <div class="news-card">
-            <!-- 썸네일 이미지 -->
-            <div class="news-thumbnail" :style="{ backgroundImage: `url('/img/member/chanmin.JPG')` }"></div>
-            <!-- 뉴스 타이틀 -->
-            <div class="news-title">
-              <h3>[새소식] 홈페이지가 리뉴얼 되었습니다.</h3>
+          <nuxt-link :to="{ name: 'board-news-nid', params: { nid: 1 }}">
+            <div class="news-card">
+              <!-- 썸네일 이미지 -->
+              <div class="news-thumbnail" :style="{ backgroundImage: `url('/img/member/chanmin.JPG')` }"></div>
+              <!-- 뉴스 타이틀 -->
+              <div class="news-title">
+                <h3>[새소식] 홈페이지가 리뉴얼 되었습니다.</h3>
+              </div>
+              <div class="news-date">2018.03.01</div>
             </div>
-            <div class="news-date">2018.03.01</div>
-          </div>
+          </nuxt-link>
+        </li>
+        <li class="col-sm-4">
+          <nuxt-link :to="{ name: 'board-news-nid', params: { nid: 1 }}">
+            <div class="news-card">
+              <!-- 썸네일 이미지 -->
+              <div class="news-thumbnail" :style="{ backgroundImage: `url('/img/member/chanmin.JPG')` }"></div>
+              <!-- 뉴스 타이틀 -->
+              <div class="news-title">
+                <h3>[새소식] 홈페이지가 리뉴얼 되었습니다.</h3>
+              </div>
+              <div class="news-date">2018.03.01</div>
+            </div>
+          </nuxt-link>
+        </li>
+        <li class="col-sm-4">
+          <nuxt-link :to="{ name: 'board-news-nid', params: { nid: 1 }}">
+            <div class="news-card">
+              <!-- 썸네일 이미지 -->
+              <div class="news-thumbnail" :style="{ backgroundImage: `url('/img/member/chanmin.JPG')` }"></div>
+              <!-- 뉴스 타이틀 -->
+              <div class="news-title">
+                <h3>[새소식] 홈페이지가 리뉴얼 되었습니다.</h3>
+              </div>
+              <div class="news-date">2018.03.01</div>
+            </div>
+          </nuxt-link>
         </li>
       </ul>
     </section>
